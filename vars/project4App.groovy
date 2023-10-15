f28e28c5f245df3d3daddc951b3de2044449802b
Kondji237 <steevengaleu@gmail.com>
2023-10-14 21:09:25 -0400

    project 4 added

---
+++ vars/project4App.groovy
@@ -0,0 +1,49 @@
+def call(String repoUrl){
+    pipeline{
+  agent any 
+  stages{
+    stage('clonecode'){
+      steps{
+        checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'team7-git-id', url: 'https://github.com/Kondji237/Project4Group6.git']])
+        }
+      }
+            stage('1st parallel'){
+      parallel{
+      stage('Divine'){
+        steps{
+          sh 'uname -a'
+          sh 'lscpu'
+        }
+      }
+      stage('Ngantcha'){
+        steps{
+          sh 'df -h'
+          sh 'free -g'
+        }
+      }
+      }
+      }
+      stage('2nd parallel'){
+      parallel{
+      stage('Steeve'){
+        steps{
+          sh 'lsblk'
+          sh 'uptime'
+        }
+      }
+      stage('Ngaleu'){
+        steps{
+          sh 'cal'
+          sh 'sudo systemctl status jenkins'
+        }
+      }
+      }
+            }
+    stage('projet4'){
+        steps{
+            echo 'Bienvenu projet 4'
+        }
+    } 
+  }
+}
+}
